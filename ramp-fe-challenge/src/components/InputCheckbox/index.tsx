bb51b23f1b3a7b281789f1b1754180f8a4859ee9
Dawoud <dawoud.mahmud@gmail.com>
2023-06-20 23:33:41 -0700

    Import project

---
+++ ramp-fe-challenge/src/components/InputCheckbox/index.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames"
+import {useRef, useState} from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+    const [isChecked, setIsChecked] = useState(checked);
+
+    const handleToggle = () => {
+        setIsChecked(!isChecked);
+        onChange(!isChecked);
+    };
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+        onClick={handleToggle}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  )
+}
